26c571ceeb69e6bec851e2292bfdcdb4fd36edcb
AYoung <ayoung@elevatodigital.com>
2022-09-22 16:08:07 -0700

    adding files

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config(); //initialize dotenv
 
 const { Client, GatewayIntentBits } = require('discord.js');
+const app = require('./app');
+const prefix = "!dl";
 
 const client = new Client(
   {
@@ -12,14 +14,21 @@ const client = new Client(
   }
 );
 
+// Function to parse what functionality the user wants
+// Hands off process to one of the App classes
+const handoff = async args => {
+
+}
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  console.log(app);
 });
 
 client.on('messageCreate', async msg => {
-  if (msg.author.bot) return;
-  const reply = await msg.reply("Some Message");
-  console.log(reply.content, msg.content);
+  if (!msg.content.startsWith(prefix)) return;
+
+  await handoff()
 });
 
 //make sure this line is the last line
