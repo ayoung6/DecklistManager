00f544964c03b7e1f56305d7d87e3f09e528128c
AYoung <ayoung@elevatodigital.com>
2022-09-26 16:34:31 -0700

    JSON-Server needed now, easier access to deck list data

--- commands/listDecklist.js
+++ commands/listDecklist.js
@@ -1,11 +1,12 @@
 const {SlashCommandBuilder, EmbedBuilder} = require('discord.js');
-const server = require(`../${process.env.JSON_DATABASE}`);
+const API = require('/Helpers/JsonServer');
 
-const listDecklists = async interaction => {
+const listDeckLists = async interaction => {
+	const deckLists = API.getAllDecks();
 	interaction.channel.send({embeds: [new EmbedBuilder()
                 .setColor('#1a8175')
                 .setTitle(`Saved Lists`)
-                .setDescription(`${JSON.stringify(server, null, 4)}`)]});
+                .setDescription(`${JSON.stringify(deckLists, null, 4)}`)]});
 };
 
 module.exports = {
@@ -13,6 +14,6 @@ module.exports = {
 		.setName('listlists')
 		.setDescription('Lists all saved decklists'),
 	async execute(interaction) {
-		await listDecklists(interaction);
+		await listDeckLists(interaction);
 	},
 };
