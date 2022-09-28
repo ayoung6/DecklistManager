1384f53add1e56e1f08f947adb18678d25b92f79
Aaron <aaronjyoung1@gmail.com>
2022-09-28 14:35:22 -0700

    Moved random logic into API file

--- commands/randomList.js
+++ commands/randomList.js
@@ -1,13 +1,10 @@
 const {SlashCommandBuilder, EmbedBuilder} = require('discord.js');
 const API = require('../Helpers/JsonServer.js');
-const crypto = require("crypto");
 
 const randomList = async interaction => {
 	await interaction.reply(`Fetching your random deck list...`);
-	const deckLists = await API.getAllDecks();
-	const index = crypto.randomInt(deckLists.length);
-	const deckList = deckLists[index];
-	const packet = {files: [{attachment: deckList.cod, name: `${deckList.name}.cod`}]};
+	const deckList = await API.getRandomDeck();
+	const packet = {files: [{attachment: deckList.file, name: `${deckList.name}.cod`}]};
 
 	if (!!!interaction.options.get('private'))
 		return await interaction.user.send(packet);
